206ba7cf19f052d6849baa4bbb4cee9d768cff54
itDarashukD <61045219+itDarashukD@users.noreply.github.com>
2020-03-04 23:18:56 +0300

    здесь вопрос

---
+++ src/com/company/Nasledovanie/Animal.java
@@ -0,0 +1,17 @@
+package company.Nasledovanie;
+
+public class Animal {
+
+    //наследование полей
+    String name="some animal";
+
+    public void eat() {
+        System.out.println("i eating");
+
+    }
+
+    public void sleep() {
+        System.out.println("i sleeping");
+
+    }
+}
