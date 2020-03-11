67538c5f7423647849521606298b10077792281f
itDarashukD <61045219+itDarashukD@users.noreply.github.com>
2020-03-12 02:24:23 +0300

    HomeWork 7 Card

--- src/com/company/dz8/Task27_4.java
+++ src/com/company/dz8/Task27_4.java
@@ -2,7 +2,11 @@ package company.dz8;
 
 import java.util.ArrayList;
 
+//27 создать коллекцию, наполнить ее случайными числами.Удалить повторяющиеся числа
 public class Task27_4 {
+    /**
+  создаем и заполняем рамдомно лист
+     */
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 15; i++) {
@@ -10,7 +14,9 @@ public class Task27_4 {
         }
         list.forEach(System.out::print);
         System.out.println();
-
+        /**
+       определяем повторы и удаляем их
+         */
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (list.get(i).equals(list.get(j))) {
