9f5f7831cd99f7734de5b643f7290ca41417e3d8
itDarashukD <61045219+itDarashukD@users.noreply.github.com>
2020-03-14 21:43:00 +0300

    HomeWork 8 Task 26,27,28,29

--- src/com/company/dz8/Task26.java
+++ src/com/company/dz8/Task26.java
@@ -15,7 +15,7 @@ public class Task26 {
         ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < 20; i++) {
-
+        
             list.add((int) (Math.random() * 10 + 1));
         }
         /**
